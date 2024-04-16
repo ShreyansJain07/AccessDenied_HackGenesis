b71ecf7f10acd3dfec7f8f9227c74cd3eee73914
Varun Jajoo <128844637+Varun-Jajoo@users.noreply.github.com>
2024-04-17 03:44:44 +0530

    Update questionsArray.js

--- frontend/src/constants/questionsArray.js
+++ frontend/src/constants/questionsArray.js
@@ -14,6 +14,7 @@ let questionsArray = [
       question: "Phone number",
       resumeField: "Phone Number",
       resumeFieldId: "phoneNumber",
+        
     },
     {
       question: "Enter your address",
