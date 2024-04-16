8a6dd7493a52fd3eaeea3ca142f8baa2edc2acfa
Siddhima22 <siddhima22@gamil.com>
2024-04-16 23:23:09 +0530

    design added

---
+++ frontend/src/screens/NewFile.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const NewFile = () => {
+  return (
+    <div>
+      <div>
+        <h1>we bring to you the best Work 
+        </h1>
+        <a>this is the newest techknology</a>
+        <p>the best techknology</p>
+        <p>it consits of many features of</p>
+        <li>ai face recognition</li>
+        <li>deep fake</li>
+        <li>one on on mentoring session </li>
+        <li>natural language processing (NLP)</li>
+        <li>computer vision applications</li>
+        <li>blockchain technology</li>
+        <li>Internet of Things (IoT) devices</li>
+        <li>cybersecurity measures</li>
+        <li>big data analytics</li>
+        <li>robotics and automation</li>
+              </div>
+    </div>
+  )
+}
+
+export default NewFile
